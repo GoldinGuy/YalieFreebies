8dc67e0d2c323959b86842ce818ef123707d3a99
GoldinGuy <sethgoldin27@gmail.com>
2022-06-30 00:43:41 -0400

    add a bunch of deals

--- src/components/deal_card.tsx
+++ src/components/deal_card.tsx
@@ -3,28 +3,48 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const DealCard = ({deal }: { deal: Deal }) => {
     return (
-			<a href={deal.url} className={`rounded-2xl p-6 shadow-md hover:shadow-sm`}>
+			<a
+				href={deal.url}
+				className={`rounded-2xl hover:shadow-lg h-fit`}
+			>
 				{/* hover:bg-gray-50 */}
-				<div
-					className={`flex items-center justify-center ${deal.icon.bgColor}  w-14 h-14 mb-4 rounded-2xl`}
-				>
-					<FontAwesomeIcon
-						icon={deal.icon.def}
-						size="2x"
-						className={`${deal.icon.color}`}
-					/>
+				<div className="px-6 ">
+					<div className="flex flex-row items-center">
+						<div
+							className={`flex items-center justify-center ${deal.icon.bgColor}  w-14 h-14 mb-4 rounded-2xl`}
+						>
+							<FontAwesomeIcon
+								icon={deal.icon.def}
+								size="2x"
+								className={`${deal.icon.color}`}
+							/>
+						</div>
+						<div className="px-2 ml-3 py-1 h-fit text-sm font-semibold max-w-fit bg-gray-100 rounded-xl mb-0">
+							{deal.discount}
+						</div>
+					</div>
+
+					<h3 className="mb-2 text-base font-semibold leading-tight text-gray-900">
+						{deal.name}
+					</h3>
 				</div>
-				<h3 className="mb-2 text-base font-semibold leading-tight text-gray-900">
-					{deal.name}
-				</h3>
-				<div className="px-2 py-1 text-sm font-semibold max-w-fit bg-gray-100 rounded-xl mb-3">
-					{deal.discount}
+				<div className="pt-3 pb-0 mb-7">
+					{deal.perks.map((p, i) => {
+						return (
+							<span
+								className={`sm:ml-4 mx-2 sm:mx-0 text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 mt-1 rounded-full cursor-pointer ${deal.icon.color} bg-gray-100`}
+								key={i}
+							>
+								{p}
+							</span>
+						);
+					})}
 				</div>
-				<ul className="text-sm text-gray-500">
+				{/* <ul className="text-sm text-gray-500">
 					{deal.perks.map((p, i) => {
 						return <li key={i}>• {p}</li>;
 					})}
-				</ul>
+				</ul> */}
 			</a>
 		);
 }
--- src/pages/api/deals.ts
+++ src/pages/api/deals.ts
@@ -1,5 +1,5 @@
-import { faAmazon, faGithub, faMicrosoft, faSpotify, faYoutube } from "@fortawesome/free-brands-svg-icons";
-import { faCalendar, faCalendarAlt, faCloud, faCode, faCoffee, faFilm, faLock, faMailBulk, faMusic, faNewspaper, faNoteSticky, faPen, faPlus, faShield, faShieldAlt, faTv, faTvAlt, faVideoCamera, IconDefinition } from "@fortawesome/free-solid-svg-icons";
+import { faAmazon, faGithub, faLinux, faMicrosoft, faSpotify, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import { faBed, faBox, faCalendar, faCalendarAlt, faCloud, faCode, faCoffee, faContactCard, faDumbbell, faFilm, faFolder, faGift, faKeyboard, faLock, faMailBulk, faMusic, faNewspaper, faNoteSticky, faPaintRoller, faPen, faPenAlt, faPlane, faPlus, faPrint, faServer, faShield, faShieldAlt, faShoppingBag, faSitemap, faStopwatch, faTree, faTv, faTvAlt, faVideoCamera, faWrench, IconDefinition } from "@fortawesome/free-solid-svg-icons";
 
 export type Deal = {
 	name: string;
@@ -19,13 +19,12 @@ export const DEALS: Deal[] = [
 	{
 		name: "Adobe Creative Cloud",
 		perks: [
-			"Adobe Photoshop",
-			"Adobe Illustrator",
-			"Adobe Premier Pro",
-			"Adobe Acrobat",
-			"Adobe Lightroom",
-			"Adobe InDesign",
-			"More Adobe stuff",
+			"Photoshop",
+			"Illustrator",
+			"Premier Pro",
+			"Acrobat",
+			"Lightroom",
+			"InDesign",
 		],
 		discount: "FREE",
 		icon: {
@@ -36,35 +35,25 @@ export const DEALS: Deal[] = [
 		url: "https://e5.onthehub.com/WebStore/OfferingDetails.aspx?o=bf40b870-cbe4-eb11-813d-000d3af41938&ws=ad730196-897d-dc11-ac48-0030485a6b08&vsro=8",
 	},
 	{
-		name: "Microsoft Office",
+		name: "Microsoft Office Suite",
 		discount: "FREE",
-		perks: [
-			"Microsoft Word",
-			"Microsoft Excel",
-			"Microsoft PowerPoint",
-			"Microsoft Publisher",
-			"Microsoft OneNote",
-			"Microsoft Outlook",
-			"More MS software",
-		],
+		perks: ["Word", "Excel", "PowerPoint", "Publisher", "OneNote", "Outlook"],
 		icon: {
 			def: faMicrosoft,
-			color: "text-teal-500",
-			bgColor: "bg-teal-100",
+			color: "text-emerald-500",
+			bgColor: "bg-emerald-100",
 		},
 		url: "https://confluence.som.yale.edu/display/SC/Microsoft+Office+365+Windows",
 	},
 	{
-		name: "Amazon Prime",
+		name: "Amazon Prime Package",
 		discount: "FREE 6m ➡ $7.99 (50% off)",
 		perks: [
-			"Free 1-day shipping",
-			"Prime Music/Video/Reading",
-			"Unlimited Grubhub free delivery",
-			"6 months free LinkedIn Premium",
-			"10% off StudentUniverse travel",
-			"3 months of Calm",
-			"1 month CourseHero",
+			"Prime",
+			"3m Calm",
+			"1m CourseHero",
+			"Grubhub free delivery",
+			"6m LinkedIn Pro",
 		],
 		icon: {
 			def: faAmazon,
@@ -76,11 +65,7 @@ export const DEALS: Deal[] = [
 	{
 		name: "XFINITY & HBO Max",
 		discount: "FREE",
-		perks: [
-			"Access Xfinity Stream",
-			"Access HBO Max",
-			"Use on any of your devices",
-		],
+		perks: ["Access Xfinity Stream", "Access HBO Max", "Use on any devices"],
 		icon: {
 			def: faTvAlt,
 			color: "text-purple-500",
@@ -88,43 +73,21 @@ export const DEALS: Deal[] = [
 		},
 		url: "https://yale.service-now.com/it?id=support_article&sys_id=4f944213db625f48973777dbbf96192b",
 	},
+
 	{
-		name: "New York Times",
+		name: "Zoom Pro",
 		discount: "FREE",
-		perks: [],
+		perks: [
+			"Unlimited meeting length",
+			"Yale Zoom Links",
+			"Higher quality meetings",
+		],
 		icon: {
-			def: faNewspaper,
-			color: "text-lime-500",
-			bgColor: "bg-lime-100",
-		},
-		url: "https://yale.service-now.com/it?id=support_article&sys_id=4f944213db625f48973777dbbf96192b",
-	},
-	{
-		name: "Wall Street Journal",
-		discount: "FREE",
-		perks: [],
-		icon: {
-			def: faNewspaper,
+			def: faVideoCamera,
 			color: "text-blue-500",
 			bgColor: "bg-blue-100",
 		},
-		url: "https://yale.service-now.com/it?id=support_article&sys_id=4f944213db625f48973777dbbf96192b",
-	},
-	{
-		name: "LeaveMeAlone",
-		discount: "20% off w/ 7GSDY9FB",
-		perks: [
-			"Mass unsubscribe from emails",
-			"Rollup your favorite newsletters",
-			"Block cold emails",
-			"Do-not-disturb email",
-		],
-		icon: {
-			def: faMailBulk,
-			color: "text-fuchsia-500",
-			bgColor: "bg-fuchsia-100",
-		},
-		url: "https://leavemealone.app",
+		url: "https://www.spotify.com/us/student/",
 	},
 	{
 		name: "Matlab",
@@ -138,41 +101,54 @@ export const DEALS: Deal[] = [
 		url: "https://e5.onthehub.com/WebStore/OfferingDetails.aspx?o=04fb2c00-4b07-ec11-813e-000d3af41938&pmv=00000000-0000-0000-0000-000000000000&ws=ad730196-897d-dc11-ac48-0030485a6b08&vsro=8",
 	},
 	{
-		name: "Github Pro",
+		name: "New York Times",
 		discount: "FREE",
 		perks: [
-			"Free Jetbrains IDEs",
-			"Domain credits",
-			"Hosting credits",
-			"Literally 10x too many to list",
-			"If you're CS get this",
+			"Unlimited articles/podcasts/videos",
+			"NYT Games",
+			"NYT WireCutter Reviews",
 		],
 		icon: {
-			def: faGithub,
-			color: "text-indigo-500",
-			bgColor: "bg-indigo-100",
+			def: faNewspaper,
+			color: "text-teal-500",
+			bgColor: "bg-teal-100",
 		},
-		url: "https://education.github.com/pack",
+		url: "https://ask.library.yale.edu/faq/174871?no_redirect=true",
 	},
 	{
-		name: "Zoom Pro",
+		name: "Washington Post",
 		discount: "FREE",
-		perks: ["Unlimited meeting length", "Higher quality meetings"],
+		perks: [
+			"Unlimited articles/podcasts/videos",
+			"Access archives",
+			"Download ebooks",
+		],
 		icon: {
-			def: faVideoCamera,
-			color: "text-blue-500",
-			bgColor: "bg-blue-100",
+			def: faNewspaper,
+			color: "text-sky-500",
+			bgColor: "bg-sky-100",
 		},
-		url: "https://www.spotify.com/us/student/",
+		url: "https://web.library.yale.edu/access-washington-post",
+	},
+	{
+		name: "Wall Street Journal",
+		discount: "FREE",
+		perks: [
+			"Unlimited articles/podcasts/videos",
+			"Audio articles",
+			"Daily puzzles",
+		],
+		icon: {
+			def: faNewspaper,
+			color: "text-amber-500",
+			bgColor: "bg-amber-100",
+		},
+		url: "https://web.library.yale.edu/access-wall-street-journal",
 	},
 	{
 		name: "Spotify Premium",
 		discount: "FREE 1m ➡ $4.99/m",
-		perks: [
-			"Access Spotify (ad-free)",
-			"Access ShowTime (ad-free)",
-			"Access Hulu (ad-supported)",
-		],
+		perks: ["Spotify (ad-free)", "ShowTime (ad-free)", "Hulu (ad-supported)"],
 		icon: {
 			def: faSpotify,
 			color: "text-green-500",
@@ -183,7 +159,7 @@ export const DEALS: Deal[] = [
 	{
 		name: "Apple Music & AppleTV+",
 		discount: "FREE 1m ➡ $5.99/m",
-		perks: ["Access Apple Music (ad-free)", "Access AppleTV (ad-free)"],
+		perks: ["Unlimited Music", "Apple Music (ad-free)", "AppleTV (ad-free)"],
 		icon: {
 			def: faMusic,
 			color: "text-pink-500",
@@ -192,10 +168,37 @@ export const DEALS: Deal[] = [
 		url: "https://support.apple.com/en-us/HT205928",
 	},
 	{
+		name: "YouTube Premium",
+		discount: "FREE 3m ➡ $6.99/m (50% off)",
+		perks: ["No ads", "Download videos", "Picture-in-picture", "Audio-only"],
+		icon: {
+			def: faYoutube,
+			color: "text-red-500",
+			bgColor: "bg-red-100",
+		},
+		url: "https://www.youtube.com/premium/student",
+	},
+	{
+		name: "Github Pro",
+		discount: "FREE",
+		perks: [
+			"Free Jetbrains IDEs",
+			"Domain/hosting",
+			"Too many to list",
+			"If you're CS get this",
+		],
+		icon: {
+			def: faGithub,
+			color: "text-indigo-500",
+			bgColor: "bg-indigo-100",
+		},
+		url: "https://education.github.com/pack",
+	},
+	{
 		name: "Notion Premium",
 		discount: "FREE",
 		perks: [
-			"Organize all your info",
+			"Organize info",
 			"Unlimited file uploads",
 			"30 day revision history",
 		],
@@ -207,28 +210,11 @@ export const DEALS: Deal[] = [
 		url: "https://www.notion.so/product/notion-for-education",
 	},
 	{
-		name: "YouTube Premium",
-		discount: "FREE 3m ➡ $6.99/m (50% off)",
-		perks: [
-			"YouTube (no ads)",
-			"Download videos for offline",
-			"Picture-in-picture mode",
-			"Keep listening while screen off",
-		],
-		icon: {
-			def: faYoutube,
-			color: "text-red-500",
-			bgColor: "bg-red-100",
-		},
-		url: "https://www.youtube.com/premium/student",
-	},
-	{
 		name: "Craft Docs",
 		discount: "$2.50/m (50% off)",
 		perks: [
-			"Unlimited text 'blocks'",
-			"50 GB of Storage",
-			"Advanced export options",
+			"50 GB Storage",
+			"Advanced exporting",
 			"Collaborate with 5 friends",
 		],
 		icon: {
@@ -239,13 +225,45 @@ export const DEALS: Deal[] = [
 		url: "https://craftdocs.typeform.com/to/Ac3dD6bi?typeform-source=support.craft.do",
 	},
 	{
+		name: "ResumeGo",
+		discount: "30% off w/ YALECOUP",
+		perks: ["Revisions", "Keyword optimizations", "LinkedIn makeover"],
+		icon: {
+			def: faContactCard,
+			color: "text-red-500",
+			bgColor: "bg-red-100",
+		},
+		url: "https://www.resumego.net",
+	},
+	{
+		name: "Resumates",
+		discount: "30% off w/ STUDENT30",
+		perks: ["Print your resume in color", "25 premium copies of resume"],
+		icon: {
+			def: faContactCard,
+			color: "text-pink-500",
+			bgColor: "bg-pink-100",
+		},
+		url: "https://resumates.co",
+	},
+	{
+		name: "Cheap Airport Parking",
+		discount: "20% off w/ YALECAP",
+		perks: ["Discounted long-term rates", "Indoor parking", "Shuttles"],
+		icon: {
+			def: faPlane,
+			color: "text-green-500",
+			bgColor: "bg-green-100",
+		},
+		url: "https://www.cheapairportparking.org",
+	},
+	{
 		name: "SurfShark VPN",
 		discount: "FREE 3m ➡ $2.21/m (83% off)",
 		perks: [
 			"Protect unlimited devices",
 			"Hide location",
-			"Block ads and malware",
-			"Prevent tracking/targeted ads",
+			"Block targeted ads/malware",
 		],
 		icon: {
 			def: faShield,
@@ -258,8 +276,7 @@ export const DEALS: Deal[] = [
 		name: "Norton 360 Security",
 		discount: "$27.29/y (68% off)",
 		perks: [
-			"Protect up to three devices",
-			"Real-time threat protection",
+			"Protect up to 3 devices",
 			"Secure VPN",
 			"50GB cloud backup",
 			"Password manager",
@@ -276,31 +293,259 @@ export const DEALS: Deal[] = [
 		discount: "$19.99/y (60% off)",
 		perks: [
 			"Encrypt your data on public WiFi",
-			"Avoid tracking from school ISP",
-			"Access content from any location",
+			"Avoid school tracking",
+			"Change location",
 		],
 		icon: {
 			def: faShieldAlt,
+			color: "text-orange-500",
+			bgColor: "bg-orange-100",
+		},
+		url: "https://e5.onthehub.com/WebStore/OfferingDetails.aspx?o=11aa1af8-3386-ea11-812a-000d3af41938&ws=ad730196-897d-dc11-ac48-0030485a6b08&vsro=8",
+	},
+	{
+		name: "Gtech Fitness",
+		discount: "5% off w/ Yale5",
+		perks: ["Weight Lifting equipment", "Cardio equipment", "mats"],
+		icon: {
+			def: faDumbbell,
+			color: "text-lime-500",
+			bgColor: "bg-lime-100",
+		},
+		url: "https://www.gtechfitness.com",
+	},
+	{
+		name: "ProFlowers",
+		discount: "15% off w/ BULLDOG15",
+		perks: [
+			"Flower bouquets",
+			"Food baskets",
+			"Candy gift baskets",
+			"Balloons",
+		],
+		icon: {
+			def: faGift,
+			color: "text-fuchsia-500",
+			bgColor: "bg-fuchsia-100",
+		},
+		url: "https://www.proflowers.com",
+	},
+	{
+		name: "Ode A La Rose",
+		discount: "10% off w/ YALE10",
+		perks: ["Bouquet deliveries", "Roses", "Peonies", "Birthday gifts"],
+		icon: {
+			def: faGift,
+			color: "text-rose-500",
+			bgColor: "bg-rose-100",
+		},
+		url: "https://www.odealarose.com",
+	},
+	{
+		name: "RoseHosting",
+		discount: "20% off w/ STUDENT20",
+		perks: [
+			"Linux VPS Hosting",
+			"SSL Certificates",
+			"Register/transfer domains",
+		],
+		icon: {
+			def: faServer,
+			color: "text-amber-500",
+			bgColor: "bg-amber-100",
+		},
+		url: "https://www.rosehosting.com",
+	},
+	{
+		name: "Hostinger",
+		discount: "$15 off w/ STUDENTS15",
+		perks: ["Shared hosting", "Linux VPS"],
+		icon: {
+			def: faServer,
+			color: "text-red-500",
+			bgColor: "bg-red-100",
+		},
+		url: "https://www.hostinger.com",
+	},
+	{
+		name: "LeaveMeAlone",
+		discount: "20% off w/ 7GSDY9FB",
+		perks: [
+			"Mass unsubscribe",
+			"Rollup newsletters",
+			"Block emails",
+			"Do-not-disturb email",
+		],
+		icon: {
+			def: faMailBulk,
+			color: "text-fuchsia-500",
+			bgColor: "bg-fuchsia-100",
+		},
+		url: "https://leavemealone.app",
+	},
+	{
+		name: "RenderForest",
+		discount: "40% off w/ Yale email",
+		perks: [
+			"Videos",
+			"Logos",
+			"Websites",
+			"Mockups",
+			"Presentations",
+			"Graphics",
+		],
+		icon: {
+			def: faTree,
+			color: "text-blue-500",
+			bgColor: "bg-blue-100",
+		},
+		url: "https://www.renderforest.com",
+	},
+	{
+		name: "PenFactory",
+		discount: "10% off w/ YALE-D10",
+		perks: [
+			"Metal pens",
+			"Plastic pens",
+			"Styluses",
+			"Highlighters",
+			"Gel pens",
+		],
+		icon: {
+			def: faPenAlt,
 			color: "text-green-500",
 			bgColor: "bg-green-100",
 		},
-		url: "https://e5.onthehub.com/WebStore/OfferingDetails.aspx?o=11aa1af8-3386-ea11-812a-000d3af41938&ws=ad730196-897d-dc11-ac48-0030485a6b08&vsro=8",
+		url: "https://www.penfactory.com",
+	},
+	{
+		name: "FoamCorePrint",
+		discount: "10% off w/ YCC10",
+		perks: [
+			"Canvas",
+			"Vinyl",
+			"Aluminum",
+			"Signs",
+			"Posters",
+			"Metal calibration targets",
+		],
+		icon: {
+			def: faPrint,
+			color: "text-pink-500",
+			bgColor: "bg-pink-100",
+		},
+		url: "https://www.foamcoreprint.com",
+	},
+	{
+		name: "BluShark Watch Straps",
+		discount: "15% off w/ SAVE15",
+		perks: ["Apple watch straps", "Rubber straps", "steel straps"],
+		icon: {
+			def: faStopwatch,
+			color: "text-teal-500",
+			bgColor: "bg-teal-100",
+		},
+		url: "https://www.blusharkstraps.com",
+	},
+	{
+		name: "GoParts",
+		discount: "10% off w/ YALE10",
+		perks: ["Replacement head/tail lights", "Replace side mirrors"],
+		icon: {
+			def: faWrench,
+			color: "text-indigo-500",
+			bgColor: "bg-indigo-100",
+		},
+		url: "https://www.go-parts.com",
 	},
 	{
 		name: "Flexibits Premium",
 		discount: "FREE 2w ➡ $2.60/m (20% off)",
 		perks: [
-			"Fantastical Pro Calendar",
-			"Sync several calendars",
-			"Schedule meetings",
-			"Sync events and tasks",
-			"CardHop Contacts app",
+			"Pro Calendar",
+			"Meetings",
+			"Sync events & tasks",
+			"CardHop Contacts",
 		],
 		icon: {
 			def: faCalendarAlt,
+			color: "text-rose-500",
+			bgColor: "bg-rose-100",
+		},
+		url: "https://flexibits.com/pricing?product=fantastical",
+	},
+	{
+		name: "Casper Sleep Inc",
+		discount: "$50 off w/ bulldogs50",
+		perks: ["Mattresses", "Pillows", "Bedding", "bed frames"],
+		icon: {
+			def: faBed,
+			color: "text-sky-500",
+			bgColor: "bg-sky-100",
+		},
+		url: "https://casper.com/mattresses/",
+	},
+	{
+		name: "JMAC Supply Co.",
+		discount: "10% off w/ YALE10",
+		perks: [
+			"Biometric readers",
+			"Gas/smoke detectors",
+			"Wires & locks",
+			"cameras",
+		],
+		icon: {
+			def: faKeyboard,
+			color: "text-orange-500",
+			bgColor: "bg-orange-100",
+		},
+		url: "https://www.jmac.com",
+	},
+
+	{
+		name: "Company Folders",
+		discount: "10% off w/ YaleEDU10",
+		perks: ["Envelopes", "Pocket folders", "reinforced folders"],
+		icon: {
+			def: faFolder,
+			color: "text-blue-500",
+			bgColor: "bg-blue-100",
+		},
+		url: "https://www.companyfolders.com/presentation-folders",
+	},
+	{
+		name: "Wallpaper Boulevard",
+		discount: "15% off w/ Yale15",
+		perks: ["Grasscloth sisals", "Floral prints", "scandanavian paper"],
+		icon: {
+			def: faPaintRoller,
+			color: "text-pink-500",
+			bgColor: "bg-pink-100",
+		},
+		url: "https://www.wallpaperboulevard.com",
+	},
+
+	{
+		name: "Couture Candy",
+		discount: "15% off w/ YCC15",
+		perks: ["(All kinds of) Dresses", "Pantsuits", "Accessories"],
+		icon: {
+			def: faShoppingBag,
+			color: "text-green-500",
+			bgColor: "bg-green-100",
+		},
+		url: "https://www.couturecandy.com",
+	},
+
+	{
+		name: "Freight Pros",
+		discount: "10% off w/ Yale10",
+		perks: ["Freight shipping", "if for some reason you need this"],
+		icon: {
+			def: faBox,
 			color: "text-red-500",
 			bgColor: "bg-red-100",
 		},
-		url: "https://flexibits.com/pricing?product=fantastical",
+		url: "https://www.freightpros.com",
 	},
 ];
