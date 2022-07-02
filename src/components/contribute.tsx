42c4f72b96caeea636c0156f35f2d6416def8f7e
GoldinGuy <sethgoldin27@gmail.com>
2022-07-02 12:30:31 -0600

    fix contribute form and analytics

--- src/components/contribute.tsx
+++ src/components/contribute.tsx
@@ -24,7 +24,7 @@ const ContributeForm = () => {
 							data-attr="deal-submission-form"
 							placeholder="Free stuff"
 							maxLength={120}
-							value=""
+							// value=""
 						/>
 					</div>
 				</div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,14 @@ import Script from 'next/script';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
 		<>
+			{process.env.NEXT_PUBLIC_UMAMI_SCRIPT_URL &&
+				process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID && (
+					<Script
+						src={process.env.NEXT_PUBLIC_UMAMI_SCRIPT_URL}
+						data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
+						strategy="lazyOnload"
+					/>
+				)}
 			<Head>
 				<title>Yalie Freebies</title>
 				<meta
@@ -24,14 +32,6 @@ function MyApp({ Component, pageProps }: AppProps) {
 				/>
 				<meta property="og:image" content={"./TheShopsatYale.jpg"} />
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-				{process.env.NEXT_PUBLIC_UMAMI_SCRIPT_URL &&
-					process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID && (
-						<Script
-							src={process.env.NEXT_PUBLIC_UMAMI_SCRIPT_URL}
-							data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
-							strategy="lazyOnload"
-						/>
-					)}
 			</Head>
 
 			<Component {...pageProps} />
